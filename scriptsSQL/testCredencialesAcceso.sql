11909c9474a4f909f5c9e40a57c2f62705978926
Angel David López Álvarez <angeldavid2k17@gmail.com>
2023-03-12 02:57:55 -0600

    Adding Credentials Access

---
+++ scriptsSQL/testCredencialesAcceso.sql
@@ -0,0 +1,3 @@
+/* ADLA230310 es el Usuario de acceso al programa, y 123456789 la contraseña */
+INSERT INTO `recepcionista` (`recID`, `nomRec`, `apRec`, `amRec`, `calleRec`, `numExtRec`, `colRec`, `cpRec`, `ciudadRec`, `rolRec`, `passRec`) VALUES ('ADLA230310', 'Angel David', 'Lopez', 'Alvarez', 'Calle Inventada', 123, 'Centro', 78700, 'Matehuala', 'Admin', '123456789');
+
